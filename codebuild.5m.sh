cae314d3ae0961a2720ade3706508621e8d177e5
Stanimal <sdbondi@users.noreply.github.com>
2018-12-19 14:22:56 +0200

    initial commit

---
+++ codebuild.5m.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env /usr/local/bin/bash
+
+ # * <bitbar.title>Codebuild status bar</bitbar.title>
+ # * <bitbar.version>v1.0</bitbar.version>
+ # * <bitbar.author>sdbondi</bitbar.author>
+ # * <bitbar.author.github>sdbondi</bitbar.author.github>
+ # * <bitbar.desc>Displays the statuses of the last N builds</bitbar.desc>
+ # * <bitbar.dependencies>bash</bitbar.dependencies>
+ # * <bitbar.dependencies>aws-sdk</bitbar.dependencies>
+ # * <bitbar.dependencies>jq</bitbar.dependencies>
+ # * <bitbar.image></bitbar.image>
+ # * <bitbar.abouturl>https://github.com/sdbondi/bitbar-coebuild</bitbar.abouturl>
+
+PROJECTS=(bigneon-api bigneon-web)
+MAX_BUILDS=5
+
+EXTRA_PATHS=
+
+export PATH='/usr/local/bin:/usr/bin:$EXTRA_PATHS:$PATH'
+
+echo "CodeBuild"
+echo "---"
+
+for p in "${PROJECTS[@]}"; do
+  echo $p
+  BUILDS=$(aws codebuild list-builds-for-project --project-name $p --max-items $MAX_BUILDS | jq '.ids' | jq -c '.[]')
+  for b in "${BUILDS[@]}"; do
+    CMD=`echo aws codebuild batch-get-builds --ids $(echo $BUILDS | xargs printf -- '\"%s\" ')`
+    RAW_PROJECT_BUILDS=$(eval $CMD | jq '.builds' | jq -c '.[]')
+    readarray -t PROJECT_BUILDS <<<"$RAW_PROJECT_BUILDS"
+    for pb in "${PROJECT_BUILDS[@]}"; do
+      echo "--$(echo $pb | jq -r '.sourceVersion') - $(echo $pb | jq -r '.buildStatus')"
+    done
+  done
+done
